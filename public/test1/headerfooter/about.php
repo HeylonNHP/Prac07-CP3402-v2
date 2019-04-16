94853b4aa92d8fd13da87de0b28f37a738529bda
Heylon White <heylon.white@my.jcu.edu.au>
2019-04-16 21:08:03 +1000

    About us

---
+++ public/test1/headerfooter/about.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Heylon
+ * Date: 16/04/2019
+ * Time: 8:05 PM
+ */
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css" >
+</head>
+<body>
+<?php
+require "header.php";
+?>
+<div id="mainBody">
+<h1>About me</h1>
+<p>Hi, my name is Heylon White. I am a local resident of Townsville and study IT at JCU.</p>
+<p>My hobbies include:
+    <ul>
+        <li>IT/Programming</li>
+        <li>Photography</li>
+        <li>Tinkering with electronics</li>
+    </ul>
+</p>
+</div>
+<?php
+require "footer.php";
+?>
+</body>
+</html>
--- public/test1/headerfooter/header.php
+++ public/test1/headerfooter/header.php
@@ -5,3 +5,45 @@
  * Date: 16/04/2019
  * Time: 7:39 PM
  */
+?>
+<style>
+    a {
+        background-color: hsl(160, 100%, 66%);
+        border-radius: 4px;
+        width: 50px;
+        height: 25px;
+        display: inline-block;
+        margin-left: 10px;
+        margin-right: 10px;
+        margin-bottom: 3px;
+        margin-top: 3px;
+    }
+    a:hover {
+        background-color: hsl(160, 100%, 50%);
+    }
+    #headerContainer {
+        width: 100%;
+        height: 145px;
+        background-color: aquamarine;
+    }
+    #titleContainer{
+        margin-left: 15px;
+    }
+    nav {
+        margin-left: 25px;
+        background-color: hsl(160, 100%, 83%);
+        border-radius: 4px;
+        margin-right: 25px;
+    }
+</style>
+<div id="headerContainer">
+    <div id="titleContainer">
+        <h1>Heylon's special PHP website</h1>
+        <p>Good things happen here</p>
+    </div>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+    </nav>
+</div>
